84cf478c10f7f442cac5d72d6408bacd9a0320dd
Stephen-Ibe <saintkalson@gmail.com>
2022-12-29 08:59:34 +0100

    setup routes and pages

---
+++ src/routes/Routes.ts
@@ -0,0 +1,9 @@
+import { useRoutes } from 'react-router';
+
+const AppRoutes = () => {
+  const allRoutes = useRoutes([]);
+
+  return allRoutes;
+};
+
+export default AppRoutes;
---
+++ src/routes/index.tsx
@@ -0,0 +1,6 @@
+import React, { FC } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+const KickOff: FC = () => <Router></Router>;
+
+export default KickOff;
