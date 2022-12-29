a5912b96bb5269084006a3b88896c51d542dd178
Stephen-Ibe <saintkalson@gmail.com>
2022-12-29 09:21:46 +0100

    added custom font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
+    fontFamily: {
+      avenirBold: ['AvenirBold', 'sans-serif'],
+      avenirRegular: ['AvenirRegular', 'sans-serif'],
+    },
   },
   plugins: [],
 };
