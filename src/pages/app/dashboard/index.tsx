85064df442a755cca013922fb82a9ea2d04f1ffa
Stephen-Ibe <saintkalson@gmail.com>
2022-12-31 01:19:48 +0100

    dashboard layout and sidebar

--- src/pages/app/dashboard/index.tsx
+++ src/pages/app/dashboard/index.tsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react';
+import { IoIosArrowDown } from 'react-icons/io';
+import { HiBriefcase } from 'react-icons/hi';
 import fetchUsersRecords from '../../../utils/apis/User';
 import TopNav from '../../../components/blocks/topnav';
 
@@ -29,7 +31,18 @@ const Dashboard = (props: Props) => {
   return (
     <>
       <TopNav />
-      <main>Hello</main>
+      <main className='flex w-full h-screen'>
+        <aside className='relative w-2/12 h-screen shadow-inner '>
+          <div className='fixed z-30 h-screen px-4 py-10'>
+            <div className='flex items-center organization gap-x-4'>
+              <HiBriefcase size={20} />
+              <p>Switch Organisation</p>
+              <IoIosArrowDown />
+            </div>
+          </div>
+        </aside>
+        <section className='relative w-full'>hello</section>
+      </main>
     </>
   );
 };
