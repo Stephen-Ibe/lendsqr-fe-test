bad03c2ff526721a30c3406f0233142b7a1a82f1
Stephen-Ibe <saintkalson@gmail.com>
2023-01-02 15:19:33 +0100

    dashboard content and stats

--- src/pages/app/dashboard/index.tsx
+++ src/pages/app/dashboard/index.tsx
@@ -32,23 +32,23 @@ const Dashboard = (props: Props) => {
   return (
     <div className='relative flex flex-col'>
       <TopNav />
-      <main className='relative flex w-full h-screen top-24'>
-        <aside className='relative w-2/12 h-screen shadow-inner'>
-          <div className='fixed h-screen px-4 py-10 z-16 '>
-            <div className='flex items-center organization gap-x-4'>
+      <main>
+        <aside>
+          <div className='sidebar_content'>
+            <div className='organization'>
               <HiBriefcase size={20} />
               <p>Switch Organisation</p>
               <IoIosArrowDown />
             </div>
           </div>
         </aside>
-        <section className='relative w-full p-12 z-2'>
+        <section className='main_content'>
           <div className='heading'>
             <h1 className='text-2xl font-extrabold'>User</h1>
           </div>
-          <div className='grid grid-cols-4 stats'>
-            <div className='card border w-[240px] h-[160px] p-4 space-y-4'>
-              <div className='p-2 bg-red-300 rounded-full card_img w-fit'>
+          <div className='stats'>
+            <div className='p-4 space-y-4 card'>
+              <div className='p-2 rounded-full card_img w-fit'>
                 <img src={UserIcon} alt='users_icon' />
               </div>
               <h4>Users</h4>
